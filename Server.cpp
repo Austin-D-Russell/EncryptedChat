329bfbd38e525f90eb457cc09ca4010d95122709
Austin Russell <austinrussell2013@gmail.com>
2017-06-15 19:00:31 -0600

    Server now allows Multithred and Recv from client

--- Server.cpp
+++ Server.cpp
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/socket.h>
-#include <string>
+#include <string.h>
 #include <iostream>
 #include <limits>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <unistd.h>
 
 using namespace std;
 
-static int MAXNUMBEROFCLIENTS = 20;
+static const int MAXNUMBEROFCLIENTS = 20;
+
+struct threadargs{
+	int clients[MAXNUMBEROFCLIENTS];
+	int connfd;
+};
 
 int menu();
 int connect(int portno);
 void *client(void *arguments);
+int broadcast(threadargs* args, char buffer[1024]);
+
 
 int main(int argc, char* argv[]){
 	int portno = menu();
@@ -43,6 +51,11 @@ int menu(){
 int connect(int portno){
 	int sockfd, option = 1;
 	struct sockaddr_in serv_addr;
+	threadargs args;
+
+	for(int i = 0; i < MAXNUMBEROFCLIENTS; ++i){
+		args.clients[i] = -1;
+	}
 
 	cout << portno << endl;
 
@@ -78,8 +91,8 @@ int connect(int portno){
 
 	//will need to change based on number of client connects
 	pthread_t thread[MAXNUMBEROFCLIENTS];
-	int i = 0; 
-
+	int threadcount = 0; 
+	int numberofclients = 0;
 	for(;;){
 		struct sockaddr_in client_addr;
 		int connfd;
@@ -89,25 +102,88 @@ int connect(int portno){
 			//do something 
 		}
 
+		args.clients[numberofclients] = connfd;
+		++numberofclients;
+		args.connfd = connfd;
+
 		cout << "Spawning New Thread" << endl;
-		if(pthread_create(&thread[i++], NULL, &client, (void *)&connfd) < 0){
+		if(pthread_create(&thread[threadcount++], NULL, &client, (void *)&args) < 0){
 			cout << "Thread Creation Error" << endl;
 			//do something with error
 		}
-		if(i == MAXNUMBEROFCLIENTS){
+		if(threadcount == MAXNUMBEROFCLIENTS){
 			cout << "Max Number of Clients Reached" << endl;
 		}
-
 	}
+	close(sockfd);
 }
 
 void *client(void *arguments){
+	threadargs *args = (threadargs *)arguments;
+	fd_set readfds;
+	struct timeval tv;
+	char buffer[1024];
+	int sockfd, n, rv, nbytes;
+
+	sockfd = args ->connfd;
+	FD_ZERO(&readfds);
+	FD_SET(sockfd, &readfds);
+
+	//Timeout client after 25min
+	tv.tv_sec = 1600;
+	n = sockfd + 1;
+	//Let Everyone know someone has Joined
+
+	//while loop const checking for recv data
+	for(;;){
+		if((rv = select(n, &readfds, NULL, NULL , &tv)) < 0){
+			cout << "Select Error on thread" << endl;
+			//do we want to exit here??
+			exit(1);
+		}
+		//zero buffer
+		bzero(buffer, sizeof(buffer));
+		if((nbytes = recv(sockfd, buffer, sizeof(buffer), 0)) < 0){
+			cout << "Error in Recv Data" << endl;
+			//do we want to exit here??
+			exit(1);
+		}
+		//check if recv whole message?
+		broadcast(args, buffer);
+
+	}
 	return NULL;
 
-	//allow sending of message and revieving of messages here
+	//allow sending of message and receieving of messages here
 	//allow link to broadcast/ private message for username
 
 }
+
+int broadcast(threadargs* args, char buffer[1024]){
+	fd_set sendfds;
+	struct timeval tv;
+	FD_ZERO(&sendfds);
+	int n, sd;
+
+	for(int i = 0; i < MAXNUMBEROFCLIENTS ; i++){
+		if(args->clients[i] != -1 && args->clients[i] != args->connfd){
+			FD_SET(args->clients[i], &sendfds);
+			n = args -> connfd + 1;
+		}
+	}
+
+	if((sd = select(n, &sendfds, NULL, NULL , &tv)) < 0){
+			cout << "Select Error on thread send" << endl;
+			//do we want to exit here??
+			exit(1);
+	}
+
+	//send
+	return 0;
+	//loop through connfds stored in args and send message
+}
+
+
 //allows mult client connects
 //recvs and broadcast
 	// socket
@@ -116,3 +192,6 @@ void *client(void *arguments){
 	//accept
 	//send()
 	//recv()
+
+//Things to keep in mind
+	// if someone leaves need to shift values in conn arry in pthread args
